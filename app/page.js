8481e9e97d146d0f48704a1fb58dc742b0010710
contact855 <contact@cgconseils.info>
2026-03-17 00:01:05 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Family Office JMC</h1>
+      <p>Interface en cours de construction.</p>
+    </div>
+  );
+}
