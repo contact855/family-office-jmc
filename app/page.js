e1774dcca748e0867ea0449e66d576bf53299575
contact855 <contact@cgconseils.info>
2026-03-16 23:18:54 +0100

    Delete app directory

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 40, fontFamily: "Arial" }}>
-      <h1>Family Office – JMC</h1>
-      <p>Interface professionnelle en cours de déploiement…</p>
-    </div>
-  );
-}
